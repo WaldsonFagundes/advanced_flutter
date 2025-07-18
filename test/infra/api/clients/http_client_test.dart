e49804d785982bdab4d3da2c623bb7e6ff67554d
waldsonfagundes <waldson.fagundes@7comm.com.br>
2025-07-18 16:08:39 -0300

    test(http_client_test): ensure HttpClient returns a Map

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:advanced_flutter/domain/entities/domain_error.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -13,7 +15,7 @@ class HttpClient {
     required this.client,
   });
 
-  Future<void> get({
+  Future<T> get<T>({
     required String url,
     Map<String, String>? headers,
     Map<String, String?>? params,
@@ -40,7 +42,7 @@ class HttpClient {
 
     switch (response.statusCode) {
       case 200:
-        break;
+        return jsonDecode(response.body);
       case 401:
         throw DomainError.sessionExpired;
       default:
@@ -75,6 +77,12 @@ void main() {
 
   setUp(() {
     client = ClientSpy();
+    client.responseJson = '''
+    {
+      "key1": "value1",
+      "key2": "value2"
+    }
+  ''';
     sut = HttpClient(client: client);
     url = anyString();
   });
@@ -167,6 +175,12 @@ void main() {
       client.simulateServerError();
       final future = sut.get(url: url);
       expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should return a Map', () async {
+      final data = await sut.get(url: url);
+      expect(data['key1'], 'value1');
+      expect(data['key2'], 'value2');
     });
   });
 }
